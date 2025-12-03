874907576aac4e9f28b97f3ebb5cc954d4e7d81b
Monster0506 <tjraklovits@gmail.com>
2025-12-03 18:23:28 -0500

    Add simple footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Landing from '$lib/Landing.svelte';
 	import AudienceSection from '$lib/AudienceSection.svelte';
 	import LocationSection from '$lib/LocationSection.svelte';
+	import Footer from '$lib/Footer.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,3 +11,4 @@
 <Landing />
 <AudienceSection />
 <LocationSection location={data.location} />
+<Footer />
