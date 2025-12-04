d2b896363b05b5c18b9868914aeb3b820c6fb5ad
Monster0506 <tjraklovits@gmail.com>
2025-12-04 08:12:01 -0500

    Fix build errors

--- src/routes/admin/redirects/+page.svelte
+++ src/routes/admin/redirects/+page.svelte
@@ -58,7 +58,7 @@
 						<th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 							Updated
 						</th>
-						<th class="px-4 py-3" />
+						<th class="px-4 py-3"></th>
 					</tr>
 				</thead>
 				<tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
