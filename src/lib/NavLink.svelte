05deb7b6aa389b278b81d7462f8e9f58667df074
Monster0506 <tjraklovits@gmail.com>
2026-09-12 09:43:00 -0400

    Restyle NavLink hover state

--- src/lib/NavLink.svelte
+++ src/lib/NavLink.svelte
@@ -1,11 +1,14 @@
-<a href={route.href || "/"} class="text-sm font-medium text-white/80 hover:text-white transition-colors">
-  {#if route.text}
-    {route.text}
-  {:else if route.img}
-    <img src={route.img} alt={route.alt || "nav item"} class="h-10 w-auto" />
-  {/if}
-</a>
-
 <script>
-  export let route;
+	export let route;
 </script>
+
+<a
+	href={route.href || '/'}
+	class="text-sm font-medium text-white/80 hover:text-hacksu-green transition-colors"
+>
+	{#if route.text}
+		{route.text}
+	{:else if route.img}
+		<img src={route.img} alt={route.alt || 'nav item'} class="h-10 w-auto" />
+	{/if}
+</a>
